0256797e5399441a6a3cc8bc9e469668444a1fec
JarikProg <44119194+JarikProg@users.noreply.github.com>
2019-01-06 12:04:18 +0200

    Sections *team* and *portfolio* are ready

--- app/js/common.js
+++ app/js/common.js
@@ -33,3 +33,21 @@ function showSlides(n) {
   }  
   slides[slideIndex-1].style.display = "block";  
 }
+
+/*$(".team__list").each(function () {
+  var childLength, x
+  childLength = $(this).find('.team__item').length;
+  
+  if (childLength == 1){
+    $(".team__item").width('100%');
+  }
+  else if (childLength == 2){
+    $(".team__item").width('calc(50% - 30px)');
+  }
+  else if (childLength == 3){
+    $(".team__item").width('calc(33% - 30px)');
+  }
+  else if (childLength == 4){
+    $(".team__item").width('calc(25% - 30px)');
+  };
+})*/
